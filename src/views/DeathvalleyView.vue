1ec15a5a120fbf7b87a1e26b5aad32491fa7b599
Zexolver <Zexolver@Disroot.org>
2025-10-28 18:44:56 -0400

    Tampering with html and css to get centered row divs (wip - incomplete)

--- src/views/DeathvalleyView.vue
+++ src/views/DeathvalleyView.vue
@@ -1,9 +1,18 @@
 <template>
+    <div class="aParent">
     <div class="Gray_Box">
         <h2 id="Index_EC">
             Deathvalley
         </h2>
         <div class="box">The hottest place on the surface of the Earth I believe</div>
         <div class="box">Do NOT go there if you hate the heat!</div>
+    </div>
+    <div class="Gray_Box">
+        <h2 id="Index_EC">
+            Deathvalley
+        </h2>
+        <div class="box">The hottest place on the surface of the Earth I believe</div>
+        <div class="box">Do NOT go there if you hate the heat!</div>
+    </div>
     </div>
 </template>
