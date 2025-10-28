71bb64853b61662029e5023c6faa6151f3bb393a
Zexolver <Zexolver@Disroot.org>
2025-10-28 09:35:24 -0400

    Add American Flag

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
         <h2 id="Index_EC">
             Cool American Places
         </h2>
+        <img id="centered-flag" src="/src/assets/images/american-flag.svg" width="50%" alt="USA Flag" ></img>
         <div class="box">Amazing places most people have heard of before.</div>
         <div class="box">And you are going to read about them regardless</div>
     </div>
