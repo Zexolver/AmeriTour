a3a83a876a6ad380d77a9353e1fdefb2d12b2506
Zexolver <Zexolver@Disroot.org>
2025-10-28 18:44:11 -0400

    Add Hawaii and Alaska images

--- src/views/AlaskaView.vue
+++ src/views/AlaskaView.vue
@@ -3,6 +3,7 @@
         <h2 id="Index_EC">
             Alaskan USA 
         </h2>
+        <img src="/src/assets/images/Alaska/R.jpg"></img>
         <div class="box">A part of the USA way up north where it is very cold.</div>
         <div class="box">Quite the opposite of Hawaii</div>
     </div>
