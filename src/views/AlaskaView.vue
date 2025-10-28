0cac7766b73454571175dd9cc99d92c7bb8938bf
Zexolver <Zexolver@Disroot.org>
2025-10-28 19:05:49 -0400

    Adjust image sizes and properties

--- src/views/AlaskaView.vue
+++ src/views/AlaskaView.vue
@@ -3,7 +3,7 @@
         <h2 id="Index_EC">
             Alaskan USA 
         </h2>
-        <img src="/src/assets/images/Alaska/R.jpg"></img>
+        <img src="/src/assets/images/Alaska/R.jpg" class="small-img"></img>
         <div class="box">A part of the USA way up north where it is very cold.</div>
         <div class="box">Quite the opposite of Hawaii</div>
     </div>
--- src/views/HawaiiView.vue
+++ src/views/HawaiiView.vue
@@ -3,7 +3,7 @@
         <h2 id="Index_EC">
             The famous islands of vacation
         </h2>
-        <img src="/src/assets/images/Hawaii/Tropical-Kamaole-Beach-Maui-Hawaii-2048x1365.jpg"></img>
+        <img src="/src/assets/images/Hawaii/Tropical-Kamaole-Beach-Maui-Hawaii-2048x1365.jpg" class="small-img"></img>
         <div class="box">A very small portion of the USA famous for its beauty</div>
         <div class="box">An exact opposite from Alaska</div>
     </div>
