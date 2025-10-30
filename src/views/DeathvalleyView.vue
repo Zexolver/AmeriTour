0a074567b94d8132344d1cf58ec107881fa0b3c5
Zexolver <Zexolver@Disroot.org>
2025-10-30 19:17:46 -0400

    Work on Deathvalley vue file page

--- src/views/DeathvalleyView.vue
+++ src/views/DeathvalleyView.vue
@@ -4,15 +4,25 @@
         <h2 id="Index_EC">
             Deathvalley
         </h2>
+        <img src="/src/assets/images/Deathvalley/National-Park-Death-Valley-California-2100-x-1400.jpg" class="small-img">
         <div class="box">The hottest place on the surface of the Earth I believe</div>
         <div class="box">Do NOT go there if you hate the heat!</div>
     </div>
     <div class="Gray_Box margins1">
         <h2 id="Index_EC">
-            Deathvalley
+            California
         </h2>
-        <div class="box">The hottest place on the surface of the Earth I believe</div>
-        <div class="box">Do NOT go there if you hate the heat!</div>
+        <img src="/src/assets/images/Deathvalley/rock-desert-death-valley-national-park-california-usa_980x650.jpg" class="small-img"></img>
+        <div class="box">Resides on a huge chunk of the USA's western coastline</div>
+        <div class="box">One of the largest states in the USA</div>
     </div>
+    </div>
+    
+    <div style="background-color: rgb(65, 65, 65);">
+        <h3 class="places-header1 box-3">Deathvalley, California</h3>
+        <div class="pad2">
+            Deathvalley, California is a fascinating place to visit for its
+            unique heat and environment.
+        </div>
     </div>
 </template>
